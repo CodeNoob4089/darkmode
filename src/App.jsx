80bfdc711d614d068e86aa7fd626367c3c4c9ae4
bongho <wqoo2190@gmail.com>
2023-07-11 10:07:39 +0900

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,7 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import {
-  DropdownLabel,
-  DropdownCheckbox,
-  StyledDropdownButton,
-  lightTheme,
-  darkTheme,
-  StyledDropdownList,
-  StyledDropdownListItem,
-  Container,
-} from "./Style";
+import React from "react";
 
-const ButtonWithDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState("");
-  const [isDarkMode, setIsDarkMode] = useState(false);
+function App() {
+  return <div>App</div>;
+}
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleItemClick = (item) => {
-    setSelectedItem(item);
-    setIsOpen(false);
-  };
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  return (
-    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-      <Container>
-        <DropdownLabel>
-          <DropdownCheckbox
-            type="checkbox"
-            checked={isOpen}
-            onChange={toggleDropdown}
-          />
-          <StyledDropdownButton>
-            {isOpen ? "구매 선택 ▲" : "구매 선택 ▼"}
-          </StyledDropdownButton>
-          {isOpen && (
-            <StyledDropdownList>
-              <StyledDropdownListItem
-                onClick={() => handleItemClick("검은색바지")}
-              >
-                검은색바지
-              </StyledDropdownListItem>
-              <StyledDropdownListItem
-                onClick={() => handleItemClick("파란색바지")}
-              >
-                파란색바지
-              </StyledDropdownListItem>
-              <StyledDropdownListItem
-                onClick={() => handleItemClick("빨간색바지")}
-              >
-                빨간색바지
-              </StyledDropdownListItem>
-            </StyledDropdownList>
-          )}
-        </DropdownLabel>
-        {selectedItem && <div>{selectedItem}</div>}
-        <button onClick={toggleDarkMode}>
-          {isDarkMode ? "라이트 모드" : "다크 모드"} 전환
-        </button>
-      </Container>
-    </ThemeProvider>
-  );
-};
-
-export default ButtonWithDropdown;
+export default App;
