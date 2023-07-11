597cf03bd0c647533ff81e79d25f3a5123542a7d
bongho <wqoo2190@gmail.com>
2023-07-11 12:25:41 +0900

    react-router-dom tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import { Link, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Products from "./Pages/Products";
+import Main from "./Pages/Main";
+import Layout from "./common/Layout";
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <div>없는페이지 입니다.</div>
+              <Link to={"/"}>
+                <button>홈으로 이동</button>
+              </Link>
+            </>
+          }
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+export default function Main() {
+  const Navigate = useNavigate();
+  return (
+    <>
+      {/* main */}
+      <main>
+        {/* 케러셀 */}
+        <section
+          style={{
+            backgroundColor: "#4E4FEB",
+            color: "white",
+            height: "300px",
+            lineHeight: "300px",
+            textAlign: "center",
+            fontSize: "24px",
+          }}
+        >
+          케러셀
+        </section>
+        {/* 여름 추천템 */}
+        <section
+          style={{
+            marginTop: "56px",
+            textAlign: "center",
+          }}
+        >
+          <h2
+            onClick={() => {
+              Navigate("/Products");
+            }}
+          >
+            🔥 여름 추천템 🔥
+          </h2>
+          <Link to="/Products">더보기</Link>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "24px",
+            }}
+          >
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#068FFF",
+              }}
+            >
+              상품1
+            </div>
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#068FFF",
+              }}
+            >
+              상품2
+            </div>
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#068FFF",
+              }}
+            >
+              상품3
+            </div>
+          </div>
+        </section>
+        {/* 추가적인 데이터 */}
+        <section
+          style={{
+            marginTop: "56px",
+            textAlign: "center",
+          }}
+        >
+          <h2>🔥 세일 상품 🔥</h2>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "24px",
+            }}
+          >
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#EEEEEE",
+              }}
+            >
+              상품1
+            </div>
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#EEEEEE",
+              }}
+            >
+              상품2
+            </div>
+            <div
+              style={{
+                width: "200px",
+                height: "240px",
+                backgroundColor: "#EEEEEE",
+              }}
+            >
+              상품3
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
